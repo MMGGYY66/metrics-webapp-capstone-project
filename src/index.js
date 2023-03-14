bfd743e7feec1f0419fbf5b32c831897979712d6
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-03-14 02:57:53 -0500

    Update App.js and index.js to display the new created elements

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import store from './redux/companiesStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
   </React.StrictMode>,
 );
-
-reportWebVitals();
