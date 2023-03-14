b50fd7102b55efb0616311efd112bafbb98e67dd
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-03-14 02:44:07 -0500

    Create details page and style it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import HomeList from './components/home/HomeList';
+import Detailspage from './components/details/Detailspage';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<HomeList />} />
+        <Route path="/details/:smbl" element={<Detailspage />} />
+
+      </Routes>
     </Router>
   );
 }
